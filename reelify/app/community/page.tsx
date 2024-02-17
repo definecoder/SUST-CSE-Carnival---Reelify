53bd8d0a15c26ee0a45ed7d2d1c1f769ba32c93c
codermehraj <codermehraj@gmail.com>
2024-02-17 20:49:28 +0600

    completed community page

--- reelify/app/community/page.tsx
+++ reelify/app/community/page.tsx
@@ -47,127 +47,214 @@ const Community: NextPage = () => {
           >
             Profile
           </Button>
-          <div className="text-slate-300 font-bold p-5">
+          <div className="text-slate-300 font-bold p-5 w-max">
             {" "}
             Following Accounts{" "}
           </div>
 
-          <div className="flex-grow flex flex-col justify-start items-center overflow-scroll mb-4 sm:mb-0">                              
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
-          <User
-            name="Ridwan Siam"
-            className="p-4"
-            description={
-              <Link href="https://twitter.com/jrgarciadev" size="sm" isExternal>
-                @ridwan_siam
-              </Link>
-            }
-            avatarProps={{
-              src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-            }}
-          />
+          <div className="flex-grow flex flex-col justify-start items-center overflow-scroll mb-4 sm:mb-0">
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
+            <User
+              name="Ridwan Siam"
+              className="p-4"
+              description={
+                <Link
+                  href="https://twitter.com/jrgarciadev"
+                  size="sm"
+                  isExternal
+                >
+                  @ridwan_siam
+                </Link>
+              }
+              avatarProps={{
+                src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+              }}
+            />
           </div>
         </div>
         <div
           className="bg-slate-800 flex-grow flex flex-col justify-start items-center overflow-scroll mb-4 sm:mb-0"
           style={{ height: "calc(100vh - 64px)" }}
         >
-          <CommunityReelCard />
+          <CommunityReelCard
+            username="John Doe"
+            description="Marie Curie was a Polish-born physicist and chemist who conducted pioneering research on radioactivity. She was the first woman to win a Nobel Prize and remains the only person to have won Nobel Prizes in two different scientific fields."
+            imgLink="https://i.pravatar.cc/150?u=b042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Marie Curie was a Polish-born physicist and chemist who conducted pioneering research on radioactivity. She was the first woman to win a Nobel Prize and remains the only person to have won Nobel Prizes in two different scientific fields. Marie Curie's contributions to science have left a lasting impact on our understanding of the atomic structure."
+          />
+          <CommunityReelCard
+            username="Jane Smith"
+            description="Isaac Newton was an English mathematician, physicist, and astronomer who is widely recognized as one of the most influential scientists of all time. His laws of motion and law of universal gravitation laid the foundation for classical mechanics."
+            imgLink="https://i.pravatar.cc/150?u=c042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Isaac Newton was an English mathematician, physicist, and astronomer who is widely recognized as one of the most influential scientists of all time. His laws of motion and law of universal gravitation laid the foundation for classical mechanics. Newton's groundbreaking work continues to be fundamental in the study of physics and astronomy."
+          />
+          <CommunityReelCard
+            username="Elena Rodriguez"
+            description="Ada Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine. She is often regarded as the world's first computer programmer."
+            imgLink="https://i.pravatar.cc/150?u=d042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Ada Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's early mechanical general-purpose computer, the Analytical Engine. She is often regarded as the world's first computer programmer. Lovelace's visionary insights into computing laid the groundwork for the digital age we live in today."
+          />
+          <CommunityReelCard
+            username="Alex Johnson"
+            description="Nikola Tesla was a Serbian-American inventor, electrical engineer, and futurist best known for his contributions to the development of alternating current (AC) electrical systems. His groundbreaking inventions and innovations have had a profound impact on modern electrical power systems."
+            imgLink="https://i.pravatar.cc/150?u=e042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Nikola Tesla was a Serbian-American inventor, electrical engineer, and futurist best known for his contributions to the development of alternating current (AC) electrical systems. His groundbreaking inventions and innovations have had a profound impact on modern electrical power systems. Tesla's visionary ideas continue to inspire researchers and engineers worldwide."
+          />
+
+          <CommunityReelCard
+            username="Emily Chang"
+            description="Stephen Hawking was an English theoretical physicist, cosmologist, and author, known for his work on black holes and the nature of the universe. Despite facing physical challenges due to ALS, he made significant contributions to theoretical physics and science communication."
+            imgLink="https://i.pravatar.cc/150?u=f042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Stephen Hawking was an English theoretical physicist, cosmologist, and author, known for his work on black holes and the nature of the universe. Despite facing physical challenges due to ALS, he made significant contributions to theoretical physics and science communication. Hawking's work continues to inspire curiosity about the mysteries of the cosmos."
+          />
+
+          <CommunityReelCard
+            username="Carlos Mendez"
+            description="Rosalind Franklin was an English biophysicist and X-ray crystallographer who made significant contributions to the understanding of the molecular structures of DNA, RNA, viruses, coal, and graphite. Her work laid the foundation for the discovery of the DNA double helix."
+            imgLink="https://i.pravatar.cc/150?u=g042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Rosalind Franklin was an English biophysicist and X-ray crystallographer who made significant contributions to the understanding of the molecular structures of DNA, RNA, viruses, coal, and graphite. Her work laid the foundation for the discovery of the DNA double helix. Franklin's groundbreaking research paved the way for advancements in molecular biology."
+          />
+
+          <CommunityReelCard
+            username="Shawon Majid"
+            description="Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born"
+            imgLink="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+            videoLink="cake.mp4"
+            promptText="Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born scientists of all time."
+          />
         </div>
       </div>
     </>
--- reelify/components/CommunityReelCard.tsx
+++ reelify/components/CommunityReelCard.tsx
@@ -1,36 +1,53 @@
+"use client";
+
 import { Avatar, Button, Link } from "@nextui-org/react";
 import { AiFillLike } from "react-icons/ai";
 import { MdModeComment } from "react-icons/md";
 import { RiDownloadCloudFill } from "react-icons/ri";
 import { FaShare } from "react-icons/fa6";
 import ReactPlayer from "react-player";
+import PropTypes from 'prop-types'
+import React, { useEffect } from "react";
 
-const CommunityReelCard = () => {
+const CommunityReelCard = (props: { username: any; description: any; imgLink: any; videoLink: any; promptText: any; }) => {    
+
+  // const username = "Shawon Majid";
+  // const description = "Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born"
+  // const imgLink = "https://i.pravatar.cc/150?u=a042581f4e29026024d";
+  // const videoLink = "cake.mp4";
+  // const promptText = "Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born scientists of all time."
+
+  const {username, description, imgLink, videoLink, promptText} = props;
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  useEffect(() => {
+    console.log("isPlaying", isPlaying);
+  }, [isPlaying]);
+
   return (
     <div className="w-5/6 flex flex-col p-4 mt-10">
       <div className="w-full flex justify-start items-start pl-[15%]">
         <div className="pr-3">
           <Avatar
-            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+            src={imgLink}
             size="lg"
           />
         </div>
         <div className="flex justify-between items-start flex-grow">
           <div className="pl-3 flex flex-col justify-start items-start flex-grow">
             <div className="font-semibold text-lg text-slate-200">
-              Shawon Majid{" "}
+              {username}
               <Link className="text-blue-300 pl-2">@shawon-majid</Link>
             </div>
             <div className="pr-12 pt-1 pb-2">
-              Albert Einstein was a German-born theoretical physicist who is
-              widely held to be one of the greatest and most influential
-              scientists of all time.
+              {description}
             </div>
             <div className="flex justify-start items-center w-full">
-              <img src="https://www.secretsofuniverse.in/wp-content/uploads/2020/04/20200324_020501_0000-864x1024.png" className="w-2/5 pt-3 pb-3 pr-5" alt="" />
+              <div className="w-2/5 pt-3 pb-3 pr-5" onMouseEnter={() => setIsPlaying(true)} onMouseLeave = {() => setIsPlaying(false)}>
+              <ReactPlayer loop={true} controls={true} playing={isPlaying} width={"300px"} height={"400px"} url={videoLink}/>              
+              </div>
               <div className="ml-12 p-8 max-h-[310px] max-w-[350] overflow-scroll bg-slate-600 rounded-3xl shadow-2xl font-mono">
-              Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential
-              scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born theoretical physicist who is widely held to be one of the greatest and most influential scientists of all time.Albert Einstein was a German-born scientists of all time.              
+                {promptText}
               </div>
             </div>
             <div className="flex gap-4 pt-6">
@@ -73,7 +90,10 @@ const CommunityReelCard = () => {
             </div>
           </div>
           <div>
-            <Button className="text-white font-semibold tracking-wider hover:text-black" variant="ghost">
+            <Button
+              className="text-white font-semibold tracking-wider hover:text-black"
+              variant="ghost"
+            >
               Follow
             </Button>{" "}
           </div>
@@ -82,5 +102,13 @@ const CommunityReelCard = () => {
     </div>
   );
 };
+
+CommunityReelCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgLink: PropTypes.string.isRequired,
+  videoLink: PropTypes.string.isRequired,
+  promptText: PropTypes.string.isRequired
+}
 
 export default CommunityReelCard;
